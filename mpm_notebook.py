da1e3f89108251e0356945a7eb28170604e5d78a
Michael P. Moran <mmutexas@gmail.com>
2019-05-28 10:01:03 -0500

    add my notebook script

---
+++ mpm_notebook.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + {"toc": true, "cell_type": "markdown"}
+# <h1>Table of Contents<span class="tocSkip"></span></h1>
+# <div class="toc"><ul class="toc-item"></ul></div>
+# -
+
+
